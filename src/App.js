4c96bd1cb16ae5c7c5cee2143a509328cc65834e
lysanney <93539047+lysanney@users.noreply.github.com>
2022-03-15 20:48:22 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        <a
-          href="https://github.com/lysanney/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source{" "}
-        </a>
-        code by Lysanne
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+
+        <footer>
+          <a
+            href="https://github.com/lysanney/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source{" "}
+          </a>
+          code by Lysanne
+        </footer>
+      </div>
     </div>
   );
 }
